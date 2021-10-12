f0daaa792fa89eebabd6ff9dd99f4ddac185f12f
Helena Nel <mailhedri@gmail.com>
2021-10-13 00:48:37 +0200

    profile loading fix

--- src/Components/ProfilePage.jsx
+++ src/Components/ProfilePage.jsx
@@ -27,7 +27,49 @@ const ProfilePage = (props) => {
   const [loading, setLoading] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
   const [profileData, setProfileData] = useState({});
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState({
+    contact: {
+      email: "",
+      tel: "",
+      cell: "",
+      insta: "",
+      whatsapp: "",
+      twitter: "",
+    },
+    basic: {
+      name: "",
+      category: "",
+      email: "",
+      shipping: false,
+      services: [],
+      username: "",
+    },
+    times: {
+      monday: { trading: true, open: "09:00", closed: "17:00" },
+      tuesday: { trading: true, open: "09:00", closed: "17:00" },
+      wednesday: { trading: true, open: "09:00", closed: "17:00" },
+      thursday: { trading: true, open: "09:00", closed: "17:00" },
+      friday: { trading: true, open: "09:00", closed: "17:00" },
+      saturday: { trading: true, open: "09:00", closed: "17:00" },
+      sunday: { trading: true, open: "09:00", closed: "17:00" },
+      public: { trading: true, open: "09:00", closed: "17:00" },
+    },
+    info: {
+      services: [],
+      bio: null,
+      img_logo: null,
+      img_user: null,
+      img_banner: null,
+    },
+    location: {
+      country: null,
+      region: null,
+      city: null,
+      zip: null,
+      street: null,
+      street_numbebr: null,
+    },
+  });
 
   const URL = process.env.REACT_APP_API_URL;
 
@@ -63,7 +105,7 @@ const ProfilePage = (props) => {
       ) : (
         <>
           <Jumbotron>
-            <Image src={userData?.info.img_logo} id="banner" />
+            <Image src={userData.info.img_logo || fishshop} id="banner" />
           </Jumbotron>
           <Container className="main mb-5">
             <Container
