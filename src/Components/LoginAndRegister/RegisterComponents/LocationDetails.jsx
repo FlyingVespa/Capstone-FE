42c7ea98d74a5c6711c4a452e8aba089fecb893f
Hedri Nel <70115535+FlyingVespa@users.noreply.github.com>
2021-11-03 09:39:29 +0200

    fetch coords

--- src/Components/LoginAndRegister/RegisterComponents/LocationDetails.jsx
+++ src/Components/LoginAndRegister/RegisterComponents/LocationDetails.jsx
@@ -1,103 +1,116 @@
 /*global google*/
 
-import React from 'react'
-
+import React from "react";
+import { Form } from "react-bootstrap";
 class LocationDetails extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = this.initialState()
-    this.handlePlaceSelect = this.handlePlaceSelect.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.autocomplete = null
+    super(props);
+    this.state = this.initialState();
+    this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.autocomplete = null;
   }
 
   componentDidMount() {
-    this.autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), {})
+    this.autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete"),
+      {}
+    );
 
-    this.autocomplete.addListener("place_changed", this.handlePlaceSelect)
+    this.autocomplete.addListener("place_changed", this.handlePlaceSelect);
   }
 
   initialState() {
     return {
-      name: '',
-      street_address: '',
-      city: '',
-      state: '',
-      zip_code: '',
-      googleMapLink: ''
-    }
+      name: "",
+      street_address: "",
+      city: "",
+      state: "",
+      zip_code: "",
+      googleMapLink: "",
+      lat: "",
+      lon:""
+    };
   }
 
   handleChange(event) {
-    this.setState({[event.target.name]: event.target.value})
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
     // this.props.dispatch(addParlor(this.state))
-    this.clearForm()
+    this.clearForm();
   }
 
   handlePlaceSelect() {
-    let addressObject = this.autocomplete.getPlace()
-    // let address = addressObject.address_components
-    console.log(addressObject)
-    // this.setState({
-    //   name: addressObject.name,
-    //   street_address: `${address[0].long_name} ${address[1].long_name}`,
-    //   city: address[4].long_name,
-    //   state: address[6].short_name,
-    //   zip_code: address[8].short_name,
-    //   googleMapLink: addressObject.url
-    // })
+    let addressObject = this.autocomplete.getPlace();
+    let address = addressObject.address_components;
+    let geo = addressObject.geometry.location;
+    console.log(addressObject);
+    console.log(geo.lat());
+    console.log(geo.lng());
+    this.setState({
+      name: addressObject.name,
+      street_address: `${address[0].long_name} ${address[1].long_name}`,
+      city: address[4].long_name,
+      state: address[6].short_name,
+      zip_code: address[7].short_name,
+      googleMapLink: addressObject.url,
+      lat: geo.lat(),
+      lon: geo.lng(),
+    });
   }
 
   render() {
-    return(
-      <div>
-        <h1>Add New Parlor</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input id="autocomplete"
+    return (
+      <>
+        <Form onSubmit={this.handleSubmit}>
+   
+          <input
+            id="autocomplete"
             className="input-field"
             ref="input"
-            type="text"/>
-            <input 
-              name={"name"}
-              value={this.state.name}
-              placeholder={"Name"}
-              onChange={this.handleChange}
-            />
-            <input 
-              name={"street_address"}
-              value={this.state.street_address}
-              placeholder={"Street Address"}
-              onChange={this.handleChange}
-            />
-            <input 
-              name={"city"}
-              value={this.state.city}
-              placeholder={"City"}
-              onChange={this.handleChange}
-            />
-            <input
-              name={"state"}
-              value={this.state.state}
-              placeholder={"State"}
-              onChange={this.handleChange}
-            />
-            <input 
-              name={"zip_code"}
-              value={this.state.zip_code}
-              placeholder={"Zipcode"}
-              onChange={this.handleChange}
-            />
-            <button onSubmit={this.handleSubmit}>Submit</button>
-        </form>
-      </div>
-    )
+            type="text"
+          />
+          <input
+            name={"name"}
+            value={this.state.name}
+            placeholder={"Name"}
+            onChange={this.handleChange}
+          />
+          <input
+            name={"street_address"}
+            value={this.state.street_address}
+            placeholder={"Street Address"}
+            onChange={this.handleChange}
+          />
+          <input
+            name={"city"}
+            value={this.state.city}
+            placeholder={"City"}
+            onChange={this.handleChange}
+          />
+          <input
+            name={"state"}
+            value={this.state.state}
+            placeholder={"State"}
+            onChange={this.handleChange}
+          />
+          <input
+            name={"zip_code"}
+            value={this.state.zip_code}
+            placeholder={"Zipcode"}
+            onChange={this.handleChange}
+          />
+        <input name={"lat"}  value={this.state.lat}   onChange={this.handleChange}></input>
+        <input name={"lon"}  value={this.state.lon}   onChange={this.handleChange}></input>
+          <button onSubmit={this.handleSubmit}>Submit</button>
+        </Form>
+      </>
+    );
   }
-
 }
 
-export default LocationDetails
+export default LocationDetails;
